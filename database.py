94aa92fbb3cb5c5c6bc793146f8d70ac31a50701
alena_pominova <asu240814@gmail.com>
2025-11-09 15:20:28 +0300

    Добавила операции с БД (создание,удаление,очистка,сохранение

--- database.py
+++ database.py
@@ -1,11 +1,144 @@
-import struct
+import json
 import os
+import csv
 import hashlib
+import pandas as pd
 from datetime import datetime
+from typing import Dict,List,Optional,Any
 HEADER_SIZE = 256
 RECORD_SIZE = 100
 MAGIC_NUMBER = b'MEDDBv1'
 RECORD_FORMAT = 'i50sfi30s12s'
+
+class DataBase:
+    def __init__(self):
+        self.current_db = None
+        self.hash_id_index = {}
+        self.hash_price_index = {}
+        self.hash_name_index = {}
+        self.metadata = {}
+    def create_db(self,name : str) -> bool:
+        try:
+            os.makedirs('data',exist_ok=True)
+            with open(f'data/{name}.csv','w',newline='',encoding='utf-8') as f:
+                writer = csv.writer(f)
+                writer.writeroe([
+                    'instrument_id',
+                    'product_name',
+                    'purchase_number',
+                    'price_per_unit',
+                    'quantity',
+                    'total_price'
+                ])
+            self.current_db = name
+            self.hash_id_index = {}
+            self.hash_price_index = {}
+            self.hash_name_index = {}
+
+            self.metadata = {
+                'name' : name,
+                'created_at' : datetime.now().isoformat(),
+                'record_count' : 0,
+                'last_update' : datetime.now().isoformat()
+            }
+            self._save_metadata()
+            print(f"Data base {name} has been created successfully")
+            return True
+        except Exception as e:
+            print(f"Failed: {e}")
+            return False
+
+    def open_db(self,name: str) -> bool:
+        try:
+            if not os.path.exists(f'data/{name}.csv'):
+                print(f"Data base {name} is not found")
+                return False
+            self.current_db = name
+            if os.path.exists(f'data/{name}_meta.json'):
+                with open(f'data/{name}_meta.json','r',encoding='utf-8') as f:
+                    self.metadata = json.load(f)
+            else:
+                self.metadata = {
+                    'name': name,
+                    'created_at': datetime.now().isoformat(),
+                    'record_count': 0
+                }
+            self._rebuild_all_indexes()
+            print(f"Data base {name} has been opened successfully")
+            return True
+        except Exception as e:
+            print(f"Failed: {e}")
+            return False
+
+    def delete_db(self,name: str) -> bool:
+        try:
+            files_to_delete = [
+                f'data/{name}.csv',
+                f'data/{name}_meta.json',
+                f'data/{name}_backup.json'
+            ]
+            success = True
+            for f_path in files_to_delete:
+                if os.path.exists(f_path):
+                    try:
+                        os.remove(f_path)
+                        print(f"File {f_path} has been deleted successfully")
+                    except Exception as e:
+                        print(f"Failed for {f_path}: {e}")
+                        success = False
+            if self.current_db == name:
+                self.current_db == None
+                self.hash_id_index = {}
+                self.hash_price_index = {}
+                self.hash_name_index = {}
+                self.metadata = {}
+            if success:
+                print(f"Data base {name} has been deleted successfully")
+            return success
+        except Exception as e:
+            print(f"Failed: {e}")
+            return False
+
+    def clear_db(self) -> bool:
+        if not self.current_db:
+            print("Data base is not opened")
+            return False
+        try:
+            with open(f'data/{self.current_db}.csv', 'w', newline='', encoding='utf-8') as f:
+                writer = csv.writer(f)
+                writer.writerow([
+                    'instrument_id',
+                    'product_name',
+                    'purchase_number',
+                    'price_per_unit',
+                    'quantity',
+                    'total_price'
+                ])
+            self.hash_id_index = {}
+            self.hash_price_index = {}
+            self.hash_name_index = {}
+            self.metadata['record_count'] = 0
+            self.metadata['last_update'] = datetime.now().isoformat()
+            self._save_metadata()
+            print(f"Data base {self.current_db} has been cleaned successfully")
+            return True
+        except Exception as e:
+            print(f"Failed: {e}")
+            return False
+    def save_db(self) -> bool:
+        if not self.current_db:
+            print("Data base is not opened")
+            return False
+        try:
+            self._save_metadata()
+            print(f"Data base {self.current_db} has been saved succesfully")
+            return True
+        except Exception as e:
+            print(f"Failed: {e}")
+            return False
+
+
+
 
 class HashIndex:
     def __init__(self,size = 1000):
@@ -38,3 +171,4 @@ class HashIndex:
             if self.table[index][0] == key:
                 return self.table[index][1]
         return None
+
